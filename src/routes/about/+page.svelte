823c3d045d44f474102caf7f448b62ae0396f98a
learnuidev <learnuidev@gmail.com>
2023-09-09 21:37:19 -0400

    Installed chadcn

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,23 @@
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button";
+  </script>
+
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About this app" />
 </svelte:head>
 
+
+
 <div class="text-column">
+
+	<div class="flex justify-end">
+		<Button 
+		on:click={() => {
+			alert('it works')
+		}}
+		class="">Hello world</Button>
+	</div>
 	<h1>About this app</h1>
 
 	<p>
---
+++ src/routes/svnote/+page.svelte
@@ -0,0 +1,10 @@
+
+<svelte:head>
+	<title>svnote</title>
+	<meta name="description" content="A notion clone written in SvelteKit" />
+</svelte:head>
+
+<div>
+	<h1 class="">svnote</h1>
+</div>
+
